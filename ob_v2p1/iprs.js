b6f29d936d7840c307dcf2e1485ea9b714f01b16
andyfmiller <theafmiller@gmail.com>
2019-10-21 16:56:08 -0700

    Remove 2.0 docs from set
    2.1 spec documents only cover badge connect api

---
+++ ob_v2p1/iprs.js
@@ -0,0 +1,12 @@
+var _iprs = [
+    { specVersion: "2.1", company: "Digital Knowledge", electionDate: "October 11, 2019", necessaryClaims: "No", type: "RF RAND (Required & Optional Elements)" },
+    { specVersion: "2.1", company: "Washington State Board for Community and Technical Colleges (WSBCTC)", electionDate: "October 4, 2019", necessaryClaims: "No", type: "RF RAND (Required & Optional Elements)" },
+    { specVersion: "2.1", company: "Credly", electionDate: "October 3, 2019", necessaryClaims: "No", type: "RF RAND (Required & Optional Elements)" },
+    { specVersion: "2.0", company: "Campus Labs", electionDate: "March 20, 2019", necessaryClaims: "No", type: "RF RAND (Required & Optional Elements)" },
+    { specVersion: "2.0", company: "Mozilla Foundation", electionDate: "March 14, 2019", necessaryClaims: "No", type: "RF RAND (Required & Optional Elements)" },
+    { specVersion: "2.0", company: "Credly", electionDate: "March 14, 2019", necessaryClaims: "No", type: "RF RAND (Required & Optional Elements)" },
+    { specVersion: "2.0", company: "Digitalme", electionDate: "March 11, 2019", necessaryClaims: "No", type: "RF RAND (Required & Optional Elements)" },
+    { specVersion: "2.0", company: "D2L Corporation", electionDate: "March 10, 2019", necessaryClaims: "No", type: "RF RAND (Required & Optional Elements)" },
+    { specVersion: "2.0", company: "Credly", electionDate: "March 18, 2018", necessaryClaims: "No", type: "RF RAND (Required Elements)" },
+    { specVersion: "2.0", company: "Pearson", electionDate: "November 21, 2017", necessaryClaims: "No", type: "RF RAND (Required Elements)" }
+];
