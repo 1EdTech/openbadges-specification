0c2319e892bb6a4a3d0d9340fb719389a5d4ba37
Bracken Mosbacker <bmosbacker@imsglobal.org>
2020-10-01 14:46:09 -0700

    first attempt at an example extension template

---
+++ ob_v2p0/extensions/template/readme.md
@@ -0,0 +1,41 @@
+# Extension Name
+
+This is an extension for [Open Badges](https://github.com/IMSGlobal/openbadges-specification).
+
+Description of what the extension does
+
+
+## Documents
+
+- [The Working document is `specification.md`](specification.md)
+- Published specification document can be found at [Open Badges Extensions](https://www.imsglobal.org/sites/default/files/Badges/OBv2p0Final/extensions/index.html)
+
+
+## Process
+
+### Extension folder setup process
+- [x] Free space
+- [ ] Copy template extension directory
+- [ ] Update readme with specific details
+- [ ] Update `respec_support/specification_wrapper.html` with appropriate [respec configuration](https://github.com/IMSGlobal/spec-central/blob/master/respec-usage.md)
+- [ ] Delete this setup todo list and move onto work steps :)
+
+
+### Work steps
+
+- [ ] Present plan and scope to Open Badges workgroup
+- [ ] Do work
+
+#### Making Contributions
+
+Explain Github workflow, or link to main spec's workflow docs. 
+
+
+### Documentation finalization steps
+
+- [ ] Update `respec_support/contributors.js` 
+- [ ] Update `respec_support/local-biblio.js` 
+- [ ] Access respec document via `respec_support/specification_wrapper.html` (needs web server to work properly) 
+- [ ] Clean up any respec errors in `specification.md` ([common markdown problems in respec](https://github.com/IMSGlobal/spec-central/blob/master/markdown-notes.md))
+- [ ] Make sure the final respec formatting and structure works well for `specification.md`
+
---
+++ ob_v2p0/extensions/template/respec_support/local-biblio.js
@@ -0,0 +1,66 @@
+var _localBiblio = {
+  "EXAMPLE11": {
+      title: "Example Specification 1.1",
+      href: "https://www.imsglobal.org/example/v1p1/",
+      publisher: "IMS Global Learning Consortium",
+      "authors": [
+        "First, Last",
+        "First, Last"
+      ]                  
+  },
+  "EXAMPLE10": {
+      title: "Example Specification 1.0",
+      href: "https://www.imsglobal.org/example/v1p0/",
+      date: "January 20, 2015",
+      publisher: "IMS Global Learning Consortium",
+      "authors": [
+        "First, Last",
+        "First, Last"
+      ]        
+  },
+  "ONEROSTER": {
+      title: "OneRoster&trade; 1.1",
+      href: "https://www.imsglobal.org/oneroster/v1p1/",
+      publisher: "IMS Global Learning Consortium",
+      "authors": [
+        "First, Last",
+        "First, Last"
+      ]        
+  }, 
+  "EXAMPLE-IMPL": {
+      title: "Example Implementation Guide 1.1",
+      href: "https://www.imsglobal.org/example/v1p1/impl/",
+      publisher: "IMS Global Learning Consortium",
+      "authors": [
+        "First, Last",
+        "First, Last"
+      ]        
+  },
+  "EXAMPLE-CERT": {
+      title: "Example Conformance Certification Guide 1.1",
+      href: "https://www.imsglobal.org/example/v1p1/cert/",
+      publisher: "IMS Global Learning Consortium",
+      "authors": [
+        "First, Last",
+        "First, Last"
+      ]        
+  },
+  "EXAMPLE-JSONSCH": {
+      title: "Example JSON Schema",
+      href: "https://purl.imsglobal.org/example/v1p1/schema.json",
+      publisher: "IMS Global Learning Consortium",
+      "authors": [
+        "First, Last",
+        "First, Last"
+      ]        
+  },
+  "EXAMPLE-ERRATA": {
+      title: "Example 1.1 Errata",
+      href: "https://www.imsglobal.org/example/v1p1/errata/",
+      publisher: "IMS Global Learning Consortium",
+      "authors": [
+        "First, Last",
+        "First, Last"
+      ]        
+  }  
+};
