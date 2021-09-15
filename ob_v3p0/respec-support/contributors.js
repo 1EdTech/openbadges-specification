0b8bcc7af18d5bce48f7c41742f3780b7a55afab
Andy Miller (IMS) <48326098+amiller-ims@users.noreply.github.com>
2021-09-15 11:56:20 -0700

    Start

---
+++ ob_v3p0/respec-support/contributors.js
@@ -0,0 +1,8 @@
+var _contributors = [
+  {name: "First Last", company: "Acme Co Inc (Sweden)", companyURL: "www.example.com", role: "Editor"},
+  {name: "Second Third", company: "Acme Co Inc (UK)", companyURL: "www.example.com", role: "Editor"},
+  {name: "Fourth Fifth", company: "Acme Co Inc (India)", companyURL: "www.example.com", role: "Working Group Co-chair"},
+  {name: "Sixth Seventh", company: "Acme Co Inc (Japan)", companyURL: "www.example.com", role: "Working Group Co-chair"},
+  {name: "Eighth Ninth", company: "Acme Co Inc (Japan)", companyURL: "www.example.com"},
+  {name: "Tenth Eleventh", company: "Acme Co Inc (Japan)", companyURL: "www.example.com"},
+];
---
+++ ob_v3p0/respec-support/local-biblio.js
@@ -0,0 +1,2 @@
+var _localBiblio = {
+};
