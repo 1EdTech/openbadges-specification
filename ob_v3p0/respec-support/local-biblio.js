16e496ffb88f27c419d7fb281f1a78e7738c251a
Andy Miller (IMS) <48326098+amiller-ims@users.noreply.github.com>
2021-12-17 15:23:00 -0800

    Remove unused biblio entries

--- ob_v3p0/respec-support/local-biblio.js
+++ ob_v3p0/respec-support/local-biblio.js
@@ -11,12 +11,6 @@ var _localBiblio = {
         "status": "IMS Base Document",
         "publisher": "IMS Global Learning Consortium"
     },
-    "OB-INFO-30": {
-        "title": "Open Badges Specification Information Model v3.0",
-        "href": "https://www.imsglobal.org/spec/ob/v3p0/InfoModel/ob_InfoModel.html",
-        "status": "IMS Base Document",
-        "publisher": "IMS Global Learning Consortium"
-    },
     "OB-BAKE-30": {
         "title": "Open Badges Specification Baking a Badge v3.0",
         "href": "https://www.imsglobal.org/spec/ob/v3p0/ImageBinding/ob_ImageBinding.html",
@@ -29,21 +23,9 @@ var _localBiblio = {
         "status": "IMS Base Document",
         "publisher": "IMS Global Learning Consortium"
     },
-    "OB-OPEN-30": {
-        "title": "Open Badges Specification OpenAPI Schema v3.0",
-        "href": "https://www.imsglobal.org/spec/ob/v3p0/schema/openapi/",
-        "status": "IMS Base Document",
-        "publisher": "IMS Global Learning Consortium"
-    },
     "OB-JSON-30": {
         "title": "Open Badges Specification JSON Schema v3.0",
         "href": "https://www.imsglobal.org/spec/ob/v3p0/schema/json",
-        "status": "IMS Base Document",
-        "publisher": "IMS Global Learning Consortium"
-    },
-    "OB-JSONLD-30": {
-        "title": "Open Badges Specification JSON-LD Context v3.0",
-        "href": "https://www.imsglobal.org/spec/ob/v3p0/",
         "status": "IMS Base Document",
         "publisher": "IMS Global Learning Consortium"
     },
