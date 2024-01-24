6c9cb1ac3a8f046211ac6fd79db837835bf626c0
Xavi Aracil <xaracil@1edtech.org>
2024-01-24 09:48:11 +0100

    Fixed bad date

--- ob_v3p0/respec-support/iprs.js
+++ ob_v3p0/respec-support/iprs.js
@@ -6,6 +6,6 @@ var _iprs = [
     { specVersion: "3.0", company: "Workday, Inc.", electionDate: "June 10, 2022", necessaryClaims: "No", type: "RF RAND (Required & Optional Elements)" },
     { specVersion: "3.0", company: "RANDA Solutions", electionDate: "June 9, 2022", necessaryClaims: "No", type: "RF RAND (Required & Optional Elements)" },
     { specVersion: "3.0", company: "Anthology", electionDate: "June 23, 2022", necessaryClaims: "No", type: "RF RAND (Required & Optional Elements)" },
-    { specVersion: "3.0", company: "Unicon", electionDate: "June 10, 202", necessaryClaims: "No", type: "RF RAND (Required & Optional Elements)" },
+    { specVersion: "3.0", company: "Unicon", electionDate: "June 10, 2022", necessaryClaims: "No", type: "RF RAND (Required & Optional Elements)" },
     { specVersion: "3.0", company: "Bowdoin College", electionDate: "June 11, 2022", necessaryClaims: "No", type: "RF RAND (Required & Optional Elements)" }
 ];
