0709eff2b52dff2a319900f29b3f3154f84f5309
Xavi Aracil <xaracil@1edtech.org>
2024-09-16 17:57:57 +0200

    Modeled extension with mps

--- extensions/issuerAccreditationExtension/v2p0/respec-support/local-biblio.js
+++ extensions/issuerAccreditationExtension/v2p0/respec-support/local-biblio.js
@@ -3,4 +3,10 @@ var _localBiblio = {
     title: "Open Badges Issuer Accreditation Extension v2.0: Errata",
     href: "https://www.imsglobal.org/spec/ob-assessment/v2p0/errata/",
   },
+  "OB-30": {
+    title: "Open Badges Specification v3.0",
+    href: "https://www.imsglobal.org/spec/ob/v3p0/",
+    status: "Candidate Final Public",
+    publisher: "1EdTech",
+  },
 };
