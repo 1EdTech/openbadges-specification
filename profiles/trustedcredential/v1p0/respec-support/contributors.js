9c929bea59fe0b173ce53aff6f8b08ca39f4bb41
Rob C <112499312+robcoyle913@users.noreply.github.com>
2026-03-02 11:12:00 -0500

    Updated Contributors

--- profiles/trustedcredential/v1p0/respec-support/contributors.js
+++ profiles/trustedcredential/v1p0/respec-support/contributors.js
@@ -1,3 +1,5 @@
 var _contributors = [
-  {name: "Xavi Aracil", company: "1Edtech", companyURL: "", role: "Editor"}
+  {name: "Nate Otto", company: "SkyBridge Skills", companyURL: "", role: "Editor"}
+    {name: "Rob Coyle", company: "1Edtech", companyURL: "", role: "Editor"}
+      {name: "Xavi Aracil", company: "1Edtech", companyURL: "", role: "Editor"}
 ];
